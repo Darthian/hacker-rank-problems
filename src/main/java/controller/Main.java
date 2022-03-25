a8f5108b3b477183b1a0aad37460c37aeb08341c
Ian Sarasty <ian_sarasty@epam.com>
2022-03-25 12:08:35 -0500

    fixing algorithm

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,35 +1,41 @@
 package controller;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] array) {
-        Arrays.stream(array).forEach(System.out::println);
-        System.out.println(solveProblem(array));
+        if(array!= null && array.length>0 && array[0].length() > 0) {
+            System.out.println(solveProblem(array));
+        } else {
+            System.out.println("");
+        }
     }
 
     public static String solveProblem(String[] array) {
         StringBuilder prefix = new StringBuilder();
-
-        prefix.append(array[0].charAt(0));
+        //prefix.append(array[0].charAt(0));
         return evaluatePrefix(array, prefix, 0, 0);
     }
 
     /*input: [
-            [q,w,e,r,t,y],
-            [q,w,g],
-            [q,w,e]
+            [qwerty],
+            [qwg],
+            [qwe]
             ]*/
     public static String evaluatePrefix(String[] array, StringBuilder prefix, Integer index, Integer kIndex) {
-        if (!prefix.toString().equals(array[index].charAt(kIndex))) {
+        System.out.println(prefix);
+
+        if(index == 0) {
+            prefix.append(array[index].charAt(kIndex));
+        }
+
+        if (prefix.toString().charAt(kIndex) != (array[index].charAt(kIndex))) {
+            prefix.deleteCharAt(prefix.length()-1);
             return prefix.toString();
         }
-        index++;
-        if (index == array.length - 1) {
-            prefix.append(array[index].charAt(kIndex));
+
+        if (index + 1 == array.length) {
             return evaluatePrefix(array, prefix, 0, ++kIndex);
         } else {
-            return evaluatePrefix(array, prefix, index, kIndex);
+            return evaluatePrefix(array, prefix, ++index, kIndex);
         }
     }
 }
--- src/test/java/controller/MainTest.java
+++ src/test/java/controller/MainTest.java
@@ -1,23 +1,44 @@
 package controller;
 
-import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Arrays;
 
 public class MainTest {
 
     private String[] input;
 
-    @After
+    @Before
     public void setUp() {
     }
 
     @Test
-    public void testMain() {
+    public void testFirstTest() {
         input = new String[]{"123fgh45", "1234oht", "1sdf"};
-        System.out.println(input.length);
         Assert.assertEquals("1", Main.solveProblem(input));
+    }
+
+    @Test
+    public void testWithoutCommonPrefixTest() {
+        input = new String[]{"dfgh", "abcd", "ab", "a"};
+        Assert.assertEquals("", Main.solveProblem(input));
+    }
+
+    @Test
+    public void testCommonPrefixTest() {
+        input = new String[]{"qwerty", "qwg", "qwe"};
+        Assert.assertEquals("qw", Main.solveProblem(input));
+    }
+
+    @Test
+    public void testEmptyTest() {
+        input = new String[]{"", "", ""};
+        Assert.assertEquals("", Main.solveProblem(input));
+    }
+
+    @Test
+    public void testEmpty2Test() {
+        input = new String[]{};
+        Assert.assertEquals("", Main.solveProblem(input));
     }
 }
